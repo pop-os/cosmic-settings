c4dbe0aa6e7dd0fb1595fe8695652aff6cac9e58
Ashley Wulber <ashley.wulber@pm.me>
2023-06-26 11:54:02 -0400

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "Settings application for the COSMIC desktop environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nix-filter, crane, fenix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.lib.${system}.overrideToolchain fenix.packages.${system}.stable.toolchain;
+        crateNameFromCargoToml = craneLib.crateNameFromCargoToml {cargoToml = ./app/Cargo.toml;};
+        pkgDef = {
+          inherit (crateNameFromCargoToml) pname version;
+          src = nix-filter.lib.filter {
+            root = ./.;
+            include = [
+              ./app
+              ./page
+              ./pages
+              ./Cargo.toml
+              ./Cargo.lock
+              ./i18n
+              ./i18n.toml
+              ./justfile
+              ./resources
+            ];
+          };
+          nativeBuildInputs = with pkgs; [
+            just
+            pkg-config
+            autoPatchelfHook
+          ];
+          buildInputs = with pkgs; [
+            libxkbcommon
+            freetype
+            fontconfig
+            expat
+            lld
+            desktop-file-utils
+            stdenv.cc.cc.lib
+           ];
+          runtimeDependencies = with pkgs; [
+            wayland
+            libglvnd # For libEGL
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly pkgDef;
+        cosmic-settings= craneLib.buildPackage (pkgDef // {
+          inherit cargoArtifacts;
+        });
+      in {
+        checks = {
+          inherit cosmic-settings;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = cosmic-settings;
+        };
+        packages.default = cosmic-settings.overrideAttrs (oldAttrs: rec {
+          installPhase = ''
+            just prefix=$out install
+          '';
+        });
+
+        devShells.default = pkgs.mkShell rec {
+          inputsFrom = builtins.attrValues self.checks.${system};
+          LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath (builtins.concatMap (d: d.runtimeDependencies) inputsFrom);
+        };
+      });
+
+  nixConfig = {
+    # Cache for the Rust toolchain in fenix
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+}
