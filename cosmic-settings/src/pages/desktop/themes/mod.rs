45e187656c3f013dcb2c5e1d31c2ddf9ddd7cb52
Eduardo Flores <edfloreshz@gmail.com>
2024-04-29 01:20:00 -0700

    Remove local theme path and usee system path instead.

--- cosmic-settings/src/pages/desktop/themes/mod.rs
+++ cosmic-settings/src/pages/desktop/themes/mod.rs
@@ -6,6 +6,8 @@ pub use page::{Message, Page};
 use crate::pages::desktop::themes::config::Theme;
 use serde::{Deserialize, Serialize};
 use std::fs;
+use std::os::unix::raw::off_t;
+use std::path::PathBuf;
 use std::sync::OnceLock;
 
 static THEMES: OnceLock<Vec<GlobalTheme>> = OnceLock::new();
@@ -20,35 +22,63 @@ pub struct GlobalTheme {
 impl GlobalTheme {
     pub fn init_themes() -> anyhow::Result<()> {
         let mut themes = vec![];
+        let xdg_data_home = std::env::var("XDG_DATA_HOME")
+            .ok()
+            .and_then(|value| {
+                if value.is_empty() {
+                    None
+                } else {
+                    Some(PathBuf::from(value))
+                }
+            })
+            .or_else(dirs::data_local_dir)
+            .map(|dir| dir.join("themes"));
 
-        for entry in fs::read_dir("resources/themes")? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                let theme_data = fs::read_to_string(path.join("theme.ron"))?;
-                let mut theme: GlobalTheme = ron::from_str(&theme_data)?;
+        let xdg_data_dirs = std::env::var("XDG_DATA_DIRS").ok();
 
-                theme.light.path = path
-                    .join(&theme.light.path)
-                    .to_str()
-                    .unwrap_or_default()
-                    .to_string();
-                theme.dark.path = path
-                    .join(&theme.dark.path)
-                    .to_str()
-                    .unwrap_or_default()
-                    .to_string();
-                theme.light.wallpaper = path
-                    .join(&theme.light.wallpaper)
-                    .to_str()
-                    .unwrap_or_default()
-                    .to_string();
-                theme.dark.wallpaper = path
-                    .join(&theme.dark.wallpaper)
-                    .to_str()
-                    .unwrap_or_default()
-                    .to_string();
-                themes.push(theme);
+        let xdg_data_dirs = xdg_data_dirs
+            .as_deref()
+            // Default from the XDG Base Directory Specification
+            .or(Some("/usr/local/share/:/usr/share/"))
+            .into_iter()
+            .flat_map(|arg| std::env::split_paths(arg).map(|dir| dir.join("themes")));
+
+        for theme_dir in xdg_data_dirs.chain(xdg_data_home) {
+            let Ok(read_dir) = fs::read_dir(&theme_dir) else {
+                continue;
+            };
+
+            for entry in read_dir.filter_map(Result::ok) {
+                let path = entry.path();
+                if path.is_dir() {
+                    let theme_file = path.join("theme.ron");
+                    if theme_file.exists() {
+                        let theme_data = fs::read_to_string(theme_file)?;
+                        let mut theme: GlobalTheme = ron::from_str(&theme_data)?;
+
+                        theme.light.path = path
+                            .join(&theme.light.path)
+                            .to_str()
+                            .unwrap_or_default()
+                            .to_string();
+                        theme.dark.path = path
+                            .join(&theme.dark.path)
+                            .to_str()
+                            .unwrap_or_default()
+                            .to_string();
+                        theme.light.wallpaper = path
+                            .join(&theme.light.wallpaper)
+                            .to_str()
+                            .unwrap_or_default()
+                            .to_string();
+                        theme.dark.wallpaper = path
+                            .join(&theme.dark.wallpaper)
+                            .to_str()
+                            .unwrap_or_default()
+                            .to_string();
+                        themes.push(theme);
+                    }
+                }
             }
         }
 
