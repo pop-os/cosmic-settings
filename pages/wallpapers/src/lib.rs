e959fedb316dd951a1cd4bb64cbfffe2cfb7e8c4
Vukašin Vojinović <150025636+git-f0x@users.noreply.github.com>
2025-08-28 12:05:03 +0200

    improv(startup_apps): match designs
    Adds an empty list item with text when there aren't any startup apps and replaces the heading with a modified description (removing the description).

--- pages/wallpapers/src/lib.rs
+++ pages/wallpapers/src/lib.rs
@@ -1,5 +1,5 @@
 pub use cosmic_bg_config::{Color, Config, Entry, Gradient, ScalingMode, Source};
-use eyre::{OptionExt, eyre};
+use eyre::eyre;
 use fast_image_resize::SrcCropping;
 use futures_lite::Stream;
 use futures_util::StreamExt;
