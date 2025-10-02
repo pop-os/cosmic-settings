4e17382b1436426ae1a84aabf290f15186db8b9c
Ashley Wulber <ashley@system76.com>
2025-10-02 11:38:26 -0400

    chore: use same laptop display check as cosmic-comp

--- cosmic-settings/src/utils.rs
+++ cosmic-settings/src/utils.rs
@@ -7,16 +7,15 @@ pub fn display_name(name: &str, physical: (u32, u32)) -> String {
     let inches = ((physical.0.pow(2) + physical.1.pow(2)) as f32).sqrt() * 0.039_370_1;
     let inches_string = format!("{inches:.1}\"");
 
-    match name {
-        "eDP-1" | "LVDS1" => {
-            fl!("display", "laptop", size = inches_string.as_str())
-        }
-        output => fl!(
+    if name.starts_with("eDP-") || name.starts_with("LVDS-") || name.starts_with("DSI-") {
+        fl!("display", "laptop", size = inches_string.as_str())
+    } else {
+        fl!(
             "display",
             "external",
             size = inches_string.as_str(),
-            output = output
-        ),
+            output = name
+        )
     }
 }
 
