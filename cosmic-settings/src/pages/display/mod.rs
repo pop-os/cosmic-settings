e8967bd8a04c1b92cc3e46fe50b1a2fe312e2fc9
Alexander Anthis <anthisalex@gmail.com>
2026-08-19 17:24:32 -0700

    made cargo formatter happy

--- cosmic-settings/src/pages/display/mod.rs
+++ cosmic-settings/src/pages/display/mod.rs
@@ -1104,7 +1104,11 @@ impl Page {
                     .arg("--pos-y")
                     .arg(itoa::Buffer::new().format(y))
                     .arg("--refresh")
-                    .arg(format!("{}.{:03}", current.refresh_rate / 1000, current.refresh_rate % 1000))
+                    .arg(format!(
+                        "{}.{:03}",
+                        current.refresh_rate / 1000,
+                        current.refresh_rate % 1000
+                    ))
                     .arg(name)
                     .arg(itoa::Buffer::new().format(current.size.0))
                     .arg(itoa::Buffer::new().format(current.size.1));
